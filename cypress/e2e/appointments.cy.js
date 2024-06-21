fd6550f0cf4771a8125abf8ae67ac1137857694e
ihleung <ihfungleung@gmail.com>
2024-06-21 09:17:54 -0400

    updated

--- cypress/e2e/appointments.cy.js
+++ cypress/e2e/appointments.cy.js
@@ -48,6 +48,25 @@ describe("Appointments", () => {
 
 });
 
+//contain is used to check if the element contains the text like <button name="save">Save</button> so we can use cy.contains("Save") to check if the button contains the text "Save"
+
+//get is used to get the element by its attribute like <button name="save">Save</button> so we can use cy.get("[name=save]") to get the button element or <input value="hello"> so we can use cy.get("input") to get the input element
+
+//should is used to check if the element has the property like <button name="save">Save</button> so we can use cy.get("[name=save]").should("exist") to check if the button element exists
+
+//click is used to click the element like <button name="save">Save</button> so we can use cy.get("[name=save]").click() to click the button element
+
+//first is getting the first element of the array of elements like <button name="save">Save</button> <button name="save">Save</button> so we can use cy.get("[name=save]").first() to get the first button element
+
+//clear is used to clear the input field like <input value="hello"> so we can use cy.get("input").clear() to clear the input field
+ 
+//type is used to type the text in the input field like <input value="hello"> so we can use cy.get("input").type("world") to type the text "world" in the input field
+
+//request is used to make the network request like GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD, CONNECT, TRACE so request("GET", "/api/debug/reset") is used to make the GET request to the "/api/debug/reset" endpoint
+
+//visit is used to visit the URL like visit("/") is used to visit the root URL kinda the main page of the application
+
+
 /*
 Summary
 Running these tests before we commit our code is easy. It is much easier and more consistent than doing the same tests manually. These tests take longer to run because they have to load a browser. It is worth it test the integration of the client and the server.
